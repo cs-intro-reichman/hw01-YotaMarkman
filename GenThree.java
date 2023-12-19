0aae33022bd8fa086e8a9c1ee644b41a168b0875
YotaMarkman <yotam.markman@post.runi.ac.il>
2023-12-19 18:53:45 +0200

    HW1

--- GenThree.java
+++ GenThree.java
@@ -1,28 +1,24 @@
+				
+				
+			
 public class GenThree
 	{
 		public static void main ( String[]args )
 		{
+			int randomNum;
 			int a = Integer.parseInt ( args[0] );
 			int b = Integer.parseInt ( args[1] );
-			int randomIntger1 = ( int )(Math.random() * (b - a + 1)) + a;
-			int randomIntger2 = ( int )(Math.random() * (b - a + 1)) + a;
-			int randomIntger3 = ( int )(Math.random() * (b - a + 1)) + a;
-			System.out.println ( randomIntger1 );
-			System.out.println ( randomIntger2 );	
-			System.out.println ( randomIntger3 );
-			int min = randomIntger1;
-			if ( min < randomIntger2 && min < randomIntger3 )
+			int min = b;
+			for ( int i = 0; i < 3; i++ )
 			{
-				System.out.println ( " The minimal generated number was " + min );
+				randomNum = ( int )(Math.random() * (b - a )) + a;
+				System.out.println ( randomNum );
+				if ( randomNum < min )
+				{
+					min = randomNum;
+				}
 			}
-			else if ( randomIntger2 < min && randomIntger2 < randomIntger3 )
-			{
-				System.out.println ( " The minimal generated number was " + randomIntger2 );
-			}
-			else
-			{
-				System.out.println ( " The minimal generated number was " + randomIntger3 );
-			}
+			System.out.println( " The minimal generated number was " + min );
 		}
 	}
 			
